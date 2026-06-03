9238f4998d77b62e8727c5ebb63951341bbf21c2
Joan Benito Britez <joan90@gmail.com>
2026-06-03 19:26:34 -0300

    ARDUINO_8stepSeq | cambios en el Random

--- ARDUINO_8stepSeq_v1/ARDUINO_8stepSeq_v1.ino
+++ ARDUINO_8stepSeq_v1/ARDUINO_8stepSeq_v1.ino
@@ -6,16 +6,16 @@ const int pinBotones[8] = {2,3,4,5,6,7,8,9};
 const int pinLeds[8] = {10,11,12,13,A0,A1,A2,A3};
 
 //DEFINO ESTADO DE PASOS Y ESCALA DEFAULT
-bool estadoPasos[8] = {true, true, true, true, true, true, true, true};
+bool estadoPasos[8] = {true, true, true, true, true, true, true, true}; //POR DEFAULT TODOS LOS PASOS ESTAN ACTIVADOS
 const int escala[8] = {261, 294, 329, 349, 392, 440, 493, 523}; //DEFINO UNA ESCALA FIJA Y ARBITRARIA DE UNA OCTAVA: C4 A C5
 int notasPlay[8] = {261, 294, 329, 349, 392, 440, 493, 523}; //DEFINO ESCALA PARA RANDOM
 
 //DEFINO INICIO DE SECUENCIADOR Y ANTIRREBOTE
-bool estadoAnterior[8] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
-unsigned long relojSeq = 0;
+bool estadoAnterior[8] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH}; //INDICO QUE TODOS LOS BOTONES ESTAN SIN PULSAR (INPUT_PULLUP)
+unsigned long relojSeq = 0; //INICIO EL CONTADOR DEL RELOJ EN EL PASO 0
 int pasoActual = 7; //LO INICIALIZO EN 7 PARA QUE EL CONTADOR LO RESETEE A CERO EN EL LOOP E INICIALICE EN EL PRIMER PASO
-bool comboAnt = false;
-unsigned long tiempoRebote[8] = {0,0,0,0,0,0,0,0};
+bool comboAnt = false;//INICIO EL SWITCH DE COMBO(RANDOM AL PRESIONAR BOTONES 1 Y 3)
+unsigned long tiempoRebote[8] = {0,0,0,0,0,0,0,0};//CONTADOR EN 0 PARA ANTIRREBOTE
 
 //DEFINO COMPORTAMIENTO INICIAL
 void setup() {
@@ -25,13 +25,13 @@ void setup() {
   delay (1000);
   //DEFINO PINES INDIVIDUALES
   pinMode(pinBuzzer, OUTPUT);
-  //DEFINO PINES DE ARRAYS
+  //DEFINO PINES CON FOR EN ARRAYS
   for (int i=0; i<8; i++) {
     pinMode(pinBotones[i], INPUT_PULLUP);
     pinMode(pinLeds[i], OUTPUT);
   }
   //GENERO SEED PARA BOTON RANDOM, LEO VOLTAJE DE PIN PARA GENERAR ALEATOREIDAD
-  randomSeed(analogRead(A6)); // CAMBIO A A6 PARA NO LEER EL VOLTAJE DEL LED QUE ESTA EN A1
+  randomSeed(analogRead(A4)); //LEO EL VALOR DEL POTENCIOMETRO DE BPM PARA TENER UN MEJOR RANDOM
 }
 
 
@@ -58,21 +58,24 @@ void loop() {
  }
  
  //LEO COMBO VIRTUAL Y COMPARO CAMBIOS
- bool comboAct = (digitalRead(pinBotones[0]) == LOW && digitalRead(pinBotones[2]) == LOW);
- if (comboAct == true && comboAnt == false){
+ bool comboAct = (digitalRead(pinBotones[0]) == LOW && digitalRead(pinBotones[2]) == LOW); //AND ENTRE BOTONES 1 y 3
+ if (comboAct == true && comboAnt == false){ //COMPARO ESTADOS, SI HUBO CAMBIO ENTRA EN EL IF
      for (int i=0; i<8; i++){
-      estadoPasos[i] = random(0, 2);
+      estadoPasos[i] = random(2); //RANDOMIZA PASOS ACTIVADOS
       notasPlay[i] = escala[random(0, 8)];
     }
    }
  comboAnt = comboAct;
 
  //LOOP SEQ
- if (millis()-relojSeq >= tiempoPaso) {// VERIFICO SI EL TIEMPO TRANSCURRIDO (DELTA) SUPERA EL INTERVALO DEL BPM
+
+
+ //CORRO EL CONTADOR DE PASOS
+ if (millis()-relojSeq >= tiempoPaso) {// VERIFICO SI EL TIEMPO TRANSCURRIDO SUPERA EL INTERVALO DEL BPM
     digitalWrite(pinLeds[pasoActual], LOW); //APAGO EL LED DEL PASO ANTERIOR
     noTone(pinBuzzer); //APAGO EL TONO DEL PASO ANTERIOR
-    pasoActual++; //SI EL TIEMPO ES MAYOR, INCREMENTO EL PASO
-    relojSeq=millis();
+    pasoActual++; //SI EL TIEMPO TRANSCURRIDO ES MAYOR A LA DURACION DEL PASO, INCREMENTO EL PASO
+    relojSeq=millis(); //TOMO VALOR DE millis() PARA COMPARAR EL TIEMPO TRANSCURRIDO
     
     //ESCRIBO BPM EN EL SERIAL CON FORMATO DE TEXTO
     Serial.print("BPM: ");
