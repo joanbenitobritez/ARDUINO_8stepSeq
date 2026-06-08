5d4153b078082d3479679533bf4f74ec439e6de8
Joan Benito Britez <joan90@gmail.com>
2026-06-08 12:56:43 -0300

    ARDUINO_8stepSeq | Added note selection function

--- ARDUINO_8stepSeq_v1/ARDUINO_8stepSeq_v1.ino
+++ ARDUINO_8stepSeq_v1/ARDUINO_8stepSeq_v1.ino
@@ -15,8 +15,13 @@ int notasPlay[8] = {261, 294, 329, 349, 392, 440, 493, 523};                    
 bool estadoAnterior[8] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH}; // INDICO QUE TODOS LOS BOTONES ESTAN SIN PULSAR (INPUT_PULLUP)
 unsigned long relojSeq = 0;                                                // INICIO EL CONTADOR DEL RELOJ EN EL PASO 0
 int pasoActual = 7;                                                        // LO INICIALIZO EN 7 PARA QUE EL CONTADOR LO RESETEE A CERO EN EL LOOP E INICIALICE EN EL PRIMER PASO
-bool comboAnt = false;                                                     // INICIO EL SWITCH DE COMBO(RANDOM AL PRESIONAR BOTONES 1 Y 3)
-unsigned long tiempoRebote[8] = {0, 0, 0, 0, 0, 0, 0, 0};                  // CONTADOR EN 0 PARA ANTIRREBOTE
+bool comboAnt = false;
+bool potSecuestrado = false;                              // INICIO EL SWITCH DE COMBO(RANDOM AL PRESIONAR BOTONES 1 Y 3)
+unsigned long tiempoRebote[8] = {0, 0, 0, 0, 0, 0, 0, 0}; // CONTADOR EN 0 PARA ANTIRREBOTE
+// DEFINO VARIABLES DE TIEMPO PARA CALCULO DE BPM Y PASOS
+int tiempoNegra;
+int BPM;
+int tiempoPaso = 120;
 
 // DEFINO COMPORTAMIENTO INICIAL
 void setup()
@@ -34,43 +39,66 @@ void setup()
     pinMode(pinLeds[i], OUTPUT);
   }
 }
-
 // LOOP GENERAL
 
 void loop()
 {
-  // DEFINO LOOP
-  // CALCULO DE BPM min 60000ms/80 BPM=750ms | CALCULO DE BPM MAX 60000ms/220 BPM = 272ms
-  int tiempoNegra = map(analogRead(pinPot), 0, 1023, 750, 272);
-  int BPM = 60000 / tiempoNegra;
-  // CALCULO DE PASOS EN CORCHEAS
-  int tiempoPaso = tiempoNegra / 2;
+  // CALCULO SECUESTRO DEL POT
+  if (potSecuestrado == false)
+  { // VERIFICO SI EL POT NO ESTA SIENDO SECUESTRADO POR UN BOTON, SI NO LO ESTA, CALCULO EL BPM Y LOS PASOS EN CORCHEAS CON EL VALOR DEL POT
+    // CALCULO DE BPM min 60000ms/80 BPM=750ms | CALCULO DE BPM MAX 60000ms/220 BPM = 272ms
+    tiempoNegra = map(analogRead(pinPot), 0, 1023, 750, 272);
+    BPM = 60000 / tiempoNegra;
+    // CALCULO DE PASOS EN CORCHEAS
+    tiempoPaso = tiempoNegra / 2;
+  }
 
-  // LEO BOTONES DE PASOS Y DETECTO ANTIRREBOTE
   for (int i = 0; i < 8; i++)
   {
     bool estadoActual = digitalRead(pinBotones[i]);
 
-    if (estadoActual == LOW && estadoAnterior[i] == HIGH && (millis() - tiempoRebote[i] > 50))
+    // guardo el tiempo del ultimo cambio de estado para cada boton
+    if (estadoActual == LOW && estadoAnterior[i] == HIGH)
     {
-      estadoPasos[i] = !estadoPasos[i];
-      tiempoRebote[i] = millis(); // Sello el tiempo exacto del impacto válido
+      tiempoRebote[i] = millis(); // GUARDO EL TIEMPO DEL ULTIMO CAMBIO DE ESTADO PARA CADA BOTON
+    }
+    if (estadoActual == LOW && estadoAnterior[i] == LOW && millis() - tiempoRebote[i] > 300)
+    {                                                                 // VERIFICO SI EL BOTON ESTA SIENDO PRESIONADO Y SI EL TIEMPO TRANSCURRIDO DESDE EL ULTIMO CAMBIO DE ESTADO ES MAYOR A 200ms (TIEMPO DE REBOTE)
+      notasPlay[i] = escala[map(analogRead(pinPot), 0, 1023, 0, 35)]; // SI EL BOTON ESTA SIENDO PRESIONADO Y EL TIEMPO DE REBOTE HA PASADO, MAPEO EL VALOR DEL POT A LA ESCALA Y LO ASIGNO AL PASO CORRESPONDIENTE
+      potSecuestrado = true;                                          // INDICO QUE EL POT ESTA SIENDO SECUESTRADO POR UN BOTON
+                                                                      // Escribe un 1 (HIGH) o un 0 (LOW) alternando cada 50 milisegundos
+      digitalWrite(pinLeds[i], (millis() / 50) % 2);
+    }
+    // ESTADO 3 (LA LIBERACIÓN): Verifico si el boton fue soltado
+    if (estadoActual == HIGH && estadoAnterior[i] == LOW)
+    {
+      // Verifico si fue un toque corto válido
+      if (millis() - tiempoRebote[i] > 50 && millis() - tiempoRebote[i] < 300)
+      {
+        estadoPasos[i] = !estadoPasos[i]; // Invierto el paso
+      }
+      potSecuestrado = false; // Siempre libero el potenciómetro al soltar
+      digitalWrite(pinLeds[i], LOW); //FUERZO APAGADO DE LED AL SOLTAR EL BOTON PARA EVITAR QUE QUEDE PRENDIDO POR EL SECUESTRO DEL POT
     }
 
-    estadoAnterior[i] = estadoActual;
-  }
+    estadoAnterior[i] = estadoActual; // ACTUALIZO LA MEMORIA DEL BOTON
+  } // <-- ESTA LLAVE CIERRA EL BUCLE FOR DE LOS BOTONES
+
+  // LEO COMBO VIRTUAL Y COMPARO CAMBIOS
+
+  // DEFINO LOOP
 
   // LEO COMBO VIRTUAL Y COMPARO CAMBIOS
   bool comboAct = (digitalRead(pinBotones[0]) == LOW && digitalRead(pinBotones[2]) == LOW); // AND ENTRE BOTONES 1 y 3
-  // GENERO SEED PARA BOTON RANDOM, LEO VOLTAJE DE PIN PARA GENERAR ALEATOREIDAD
-  randomSeed(millis()); // LEO EL VALOR DEL POTENCIOMETRO DE BPM PARA TENER UN MEJOR RANDOM
+
   if (comboAct == true && comboAnt == false)
-  { // COMPARO ESTADOS, SI HUBO CAMBIO ENTRA EN EL IF
+  {                       // COMPARO ESTADOS, SI HUBO CAMBIO ENTRA EN EL IF
+    randomSeed(millis()); // // GENERO SEED PARA BOTON RANDOM, LEO millis() PARA QUE SEA UNA SEMILLA CAMBIANTE CADA VEZ QUE SE PRESIONA EL COMBO
     for (int i = 0; i < 8; i++)
     {
 
       estadoPasos[i] = random(2); // RANDOMIZA PASOS ACTIVADOS
-      notasPlay[i] = escala[random(0, 8)];
+      notasPlay[i] = escala[random(36)];
     }
   }
   comboAnt = comboAct;
